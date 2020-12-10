3ac3445c214ebcdfe06063d46c9f54c1975c0026
Michael Campos <camposmj89@gmail.com>
2020-12-10 12:21:36 -0800

    Setup UI for timer countdown

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -9,7 +9,44 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var timer : Timer?
+    var secondsRemaining : Int!
+    let eggTimes : [String: Int] = [
+        "Soft": 5,
+        "Medium": 7,
+        "Hard": 12
+    ]
+    var totalTime = 0
+    var secondsPassed = 0
     
-
-
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        let hardness = sender.currentTitle!
+        totalTime = eggTimes[hardness]!
+        
+        progressBar.progress = 0.0
+        secondsPassed = 0
+        titleLabel.text = hardness
+        
+        // Invalidate previously existing timer
+        timer?.invalidate()
+        
+        // Start new timer
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(fireTimer), userInfo: nil, repeats: true)
+    }
+    
+    @objc func fireTimer() {
+        if secondsPassed < totalTime {
+            secondsPassed += 1
+            
+            let percentageProgress = Float(secondsPassed)/Float(totalTime)
+            
+            progressBar.progress = percentageProgress
+        } else {
+            timer?.invalidate()
+            titleLabel.text = "Done!"
+        }
+    }
 }
